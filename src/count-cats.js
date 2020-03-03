587426ee86b45c1aa9eeae0460e87c6252bf2643
Galyna Stegniy <galyna.stegniy@gmail.com>
2020-03-03 21:36:16 +0200

    feat: count-cats task solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  return backyard.reduce((acc, row) => {
+    return (
+      acc +
+      row.reduce((acc, el) => {
+        if (el === '^^') {
+          return acc + 1;
+        } else {
+          return acc;
+        }
+      }, 0)
+    );
+  }, 0);
 };
