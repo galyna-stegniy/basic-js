84288d9972f4f36118a78331aa81562a648f7f87
Galyna Stegniy <galyna.stegniy@gmail.com>
2020-03-04 20:49:00 +0200

    feat: dream-team task solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let membersArray = members.reduce((acc, el) => {
+    if (typeof el === 'string') {
+      acc.push(el.trim()[0].toUpperCase());
+    }
+    return acc;
+  }, []);
+  return membersArray.sort().join('');
 };
